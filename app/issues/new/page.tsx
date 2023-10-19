8955e7c8fd9eaee49abe7175d1f4963d0a2c2bfb
shessafridi <mshesskhan@gmail.com>
2023-10-19 16:16:48 +0500

    Handle Errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,10 +4,14 @@ import 'easymde/dist/easymde.min.css';
 
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
+import { BiErrorCircle } from 'react-icons/bi';
 import SimpleMDE from 'react-simplemde-editor';
 
-import { Button, TextFieldInput, TextFieldRoot } from '@radix-ui/themes';
+import {
+    Button, CalloutIcon, CalloutRoot, CalloutText, TextFieldInput, TextFieldRoot
+} from '@radix-ui/themes';
 
 type NewIssue = {
   title: string;
@@ -20,14 +24,28 @@ function NewIssuePage({}: Props) {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<NewIssue>();
 
+  const [error, setError] = useState('');
+
   return (
     <form
       className='max-w-xl space-y-3'
       onSubmit={handleSubmit(async data => {
-        await axios.post('/api/issues', data);
-        router.push('/issues');
+        try {
+          await axios.post('/api/issues', data);
+          router.push('/issues');
+        } catch (error) {
+          setError('An unexpected error occured.');
+        }
       })}
     >
+      {error && (
+        <CalloutRoot color='red'>
+          <CalloutIcon>
+            <BiErrorCircle />
+          </CalloutIcon>
+          <CalloutText>{error}</CalloutText>
+        </CalloutRoot>
+      )}
       <TextFieldRoot>
         <TextFieldInput placeholder='Title' {...register('title')} />
       </TextFieldRoot>
