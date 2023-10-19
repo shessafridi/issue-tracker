8505cf52fb3b888c310db12448c23ebf29dffae5
shessafridi <mshesskhan@gmail.com>
2023-10-19 17:07:37 +0500

    Improved save issue UX

---
+++ app/components/Spinner.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/lib/utils';
+
+type Props = { className: string };
+
+function Spinner({ className }: Props) {
+  return (
+    <div
+      className={cn(
+        'inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]',
+        className
+      )}
+      role='status'
+    >
+      <span className='!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]'>
+        Loading...
+      </span>
+    </div>
+  );
+}
+
+export default Spinner;
