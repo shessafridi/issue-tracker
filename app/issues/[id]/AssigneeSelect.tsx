31cac33545e69da8a9c0381e71a4a9725d96db93
shessafridi <mshesskhan@gmail.com>
2023-10-21 16:50:57 +0500

    Shot toast notification

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,5 +1,6 @@
 'use client';
 import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
 
 import { Skeleton } from '@/app/components';
 import userService from '@/lib/api/user-service';
@@ -29,28 +30,35 @@ function AssigneeSelect({ issue }: { issue: Issue }) {
 
   if (error) return null;
 
+  const handleAssignUser = (userId: string): void => {
+    axios
+      .patch('/api/issues/' + issue.id, {
+        assignedToUserId: userId === 'un-assigned' ? null : userId,
+      })
+      .catch(() => toast.error('User could not be assigned.'));
+  };
+
   return (
-    <SelectRoot
-      defaultValue={issue.assignedToUserId || 'un-assigned'}
-      onValueChange={userId => {
-        axios.patch('/api/issues/' + issue.id, {
-          assignedToUserId: userId === 'un-assigned' ? null : userId,
-        });
-      }}
-    >
-      <SelectTrigger placeholder='Assign...' />
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Suggestions</SelectLabel>
-          <SelectItem value='un-assigned'>Unassigned</SelectItem>
-          {users?.map(user => (
-            <SelectItem key={user.id} value={user.id}>
-              {user.name}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </SelectRoot>
+    <>
+      <SelectRoot
+        defaultValue={issue.assignedToUserId || 'un-assigned'}
+        onValueChange={handleAssignUser}
+      >
+        <SelectTrigger placeholder='Assign...' />
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Suggestions</SelectLabel>
+            <SelectItem value='un-assigned'>Unassigned</SelectItem>
+            {users?.map(user => (
+              <SelectItem key={user.id} value={user.id}>
+                {user.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </SelectRoot>
+      <Toaster />
+    </>
   );
 }
 
